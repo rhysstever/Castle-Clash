1b395e87477ea6530afa8f8c9fbe40254dc7b4ab
Rhys Stever <rhys@stever.com>
2024-04-14 18:44:43 -0400

    Added randomly timed animations to map objects

---
+++ Assets/Scripts/GameObjects/RandomAnimate.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimate : MonoBehaviour
+{
+    [SerializeField]
+    private float minTime, maxTime;
+    [SerializeField]
+    private string triggerName;
+    private float timeToAnimate, currentTime;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetNewAnimateTime();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(GameManager.instance.CurrentMenuState == MenuState.Game)
+        {
+            currentTime += Time.deltaTime;
+            if(currentTime >= timeToAnimate)
+            {
+                animator.SetTrigger(triggerName);
+                currentTime = 0;
+                SetNewAnimateTime();
+            }
+        }
+    }
+
+    private void SetNewAnimateTime()
+    {
+        timeToAnimate = Random.Range(minTime, maxTime);
+    }
+}
